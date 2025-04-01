fd6c8997bf51fe62ccd345317c3aaa7945c1a726
ChichoOMG <162007542+ChichoOMG@users.noreply.github.com>
2025-04-01 01:02:47 -0700

    prueba de sw con sw en root

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ app.mount('#app');
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/public/service-worker.js')
+      navigator.serviceWorker.register('/service-worker.js')
         .then((reg) => console.log('Service Worker registrado:', reg))
         .catch((err) => console.log('Error en SW:', err));
     });
